0f37947f56735c55fea80137153746db8a2f6cc3
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-28 12:33:13 -0700

    pycodstyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,12 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = '0.0.0.0:5000' 
+app.config['SERVER_NAME'] = '0.0.0.0:5000'
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+
 
 app.run()
