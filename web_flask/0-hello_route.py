5eea3a2336488f9f2bdcac3edc35f70775070437
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-28 13:18:59 -0700

    added if nmae

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,6 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = '0.0.0.0:5000'
 
 
 @app.route('/', strict_slashes=False)
@@ -13,4 +12,5 @@ def hello():
     return "Hello HBNB!"
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,7 +4,6 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = '0.0.0.0:5000'
 
 
 @app.route('/', strict_slashes=False)
@@ -19,4 +18,5 @@ def hbnb():
     return "HBNB"
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
